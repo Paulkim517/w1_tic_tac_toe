32c22a670da4d6e7b4a12f234e4b8d352a27e022
Paul Kim <paulkim517@gmail.com>
2015-06-27 07:41:31 -0700

    added some css styling and the boxes now change color when clicked and a X and O show up

--- ticTacToe.js
+++ ticTacToe.js
@@ -2,4 +2,42 @@
 window.addEventListener("DOMContentLoaded", function() {
   // all code goes here
 
+
+
+ var boxesChoice = document.getElementsByClassName("box")
+
+var counter = 0
+
+ for(i=0;i<boxesChoice.length;i++){
+  boxesChoice[i].addEventListener("click", function(){
+ 	console.log("it works");
+ 	event.preventDefault();
+ 	
+  if(){
+
+
+  }else{
+  	
+ 	if(counter % 2 == 0){
+ 	this.style.backgroundColor="green";
+ 	this.innerText="X";
+ 	console.log("it's even")
+}else{
+	this.style.backgroundColor="black";
+  this.innerText="O";
+ 	console.log("it's odd");
+};}
+
+counter++;
+
+ 	});
+
+
+}
+
 });
+
+
+
+	
+
