281358a73206b131868482c55475842d8ddbd3e5
Paul Kim <paulkim517@gmail.com>
2015-06-28 00:25:52 -0700

    Added alert that tell player to play. added reset button that clears green and black. also made it so player can't click on already clicked box

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,11 +1,9 @@
 // wait for the DOM to finish loading
 window.addEventListener("DOMContentLoaded", function() {
-  // all code goes here
 
 
-
- var boxesChoice = document.getElementsByClassName("box")
-
+var input = document.getElementById("resetBtn");
+var boxesChoice = document.getElementsByClassName("box")
 var counter = 0
 
  for(i=0;i<boxesChoice.length;i++){
@@ -13,29 +11,46 @@ var counter = 0
  	console.log("it works");
  	event.preventDefault();
  	
-  if(){
-
-
-  }else{
-  	
- 	if(counter % 2 == 0){
+//determines whether or not greenX or blackO  	
+ 	if((counter % 2 === 0 )&&(this.innerText==="")){
  	this.style.backgroundColor="green";
  	this.innerText="X";
- 	console.log("it's even")
-}else{
+ 	counter++;
+ 	alert("Player 2's turn!");
+ 	
+}else if((counter % 2!== 0)&&(this.innerText==="")){
 	this.style.backgroundColor="black";
   this.innerText="O";
- 	console.log("it's odd");
-};}
-
-counter++;
-
- 	});
-
-
-}
+  counter++;
+  alert("Player 1's turn!");
+ 	
+}else{
+   alert("NOPE CHOOSE ANOTHER ONE!!!")
+	};
 
 });
+
+	}
+
+//supposed to be reset button
+input.addEventListener("click", function(event) {
+     for (i=0; i<boxesChoice.length; i++) { 
+        boxesChoice[i].classList.remove("x");
+        boxesChoice[i].classList.remove("o")
+        boxesChoice[i].innerText = "";
+        boxesChoice[i].style.backgroundColor = "transparent";
+
+
+        count = 0;
+        }
+        alert("LET'S START OVER");
+    });
+	
+});
+
+
+
+
 
 
 
